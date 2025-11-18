fdf9ddb0b91426d80278d82bc192a728454d4034
Your Name <you@example.com>
2025-11-18 19:46:51 +0000

    changes to nav sticky, video, responsiveness

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,6 +1,6 @@
 export default function Games() {
   return (
-    <section id="games" className=" bg-card scroll-mt-24 py-10 rounded-3xl">
+    <section id="games" className=" bg-card scroll-mt-36 px-6 py-10 mb-10 xl:scroll-mt-3 rounded-3xl">
       <h2 className="text-lg font-semibold">Games</h2>
       <div className=" px-4 mt-4 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 3 }).map((_, i) => (
