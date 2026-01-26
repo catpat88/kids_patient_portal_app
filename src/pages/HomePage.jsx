e4514ad1b92ef232e5746fe81c365cb49497f67b
Your Name <you@example.com>
2026-01-26 09:38:48 +0000

    home page layout fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,42 +15,39 @@ export default function HomePage(){
         <div className="h-px w-full bg-gray-300"></div>
       </header>
 
-      {/* Content */}
-      <main className="mx-auto max-w-6xl px-4 pb-24 pt-10 w-full">
-        <div className="grid md:grid-cols-2 items-center gap-12 md:gap-16">
-          {/* Mascot */}
-          <div className="order-2 md:order-1 flex justify-center">
-            <div className="grid place-items-center overflow-hidden w-80 h-80 md:w-90 md:h-90">
-              <img
-                src="/images/heroIMG.png"
-                alt="Hippo doctor mascot"
-                className="w-80 h-80 md:w-90 md:h-90 object-contain"
-              />
-            </div>
-          </div>
-          <div>
-            
-          </div>
-          <div className="flex flex-col sm:flex-row items-center gap-4 mt-8">
-            <h1>
-              Welcome to The Healthy Hippo Hub App
-            </h1>
-          <Link
-            to="/public-map"
-            className="inline-block rounded-full bg-hippoBlue text-ink font-semibold px-5 py-2 shadow-soft hover:brightness-[.97] active:translate-y-[1px] transition text-center"
-          >
-            View Hospital Map
-          </Link>
+ <div className="w-full max-w-4xl mx-auto">
+  <div className="flex flex-col md:flex-row items-center justify-center gap-12">
+    {/* Mascot */}
+    <img
+      src="/images/heroIMG.png"
+      alt="Hippo doctor mascot"
+      className="w-80 h-80 md:w-96 md:h-96 object-contain"
+    />
 
-          <Link
-            to="/login"
-            className="inline-block rounded-full bg-hippoBlue text-ink font-semibold px-5 py-2 shadow-soft hover:brightness-[.97] active:translate-y-[1px] transition text-center"
-          >
-            Continue To Login
-          </Link>
-        </div>
-        </div>
-      </main>
+    {/* Right side */}
+    <div className="flex flex-col items-center md:items-start gap-4 text-center md:text-left">
+      <h1 className="text-2xl font-semibold pb-12">
+        Welcome to The Healthy Hippo Hub App
+      </h1>
+
+      <Link
+        to="/public-map"
+        className="w-56 text-center rounded-full bg-hippoBlue text-ink font-semibold px-6 py-2 shadow-soft hover:brightness-[.97] transition"
+      >
+        View Hospital Map
+      </Link>
+
+      <Link
+        to="/login"
+        className="w-56 text-center rounded-full bg-hippoBlue text-ink font-semibold px-6 py-2 shadow-soft hover:brightness-[.97] transition"
+      >
+        Continue To Login
+      </Link>
+    </div>
+  </div>
+</div>
+
+
     </div>
   );
 }
